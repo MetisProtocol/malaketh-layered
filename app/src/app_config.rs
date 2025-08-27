fe2f12b4b139e088bcc92dfdf0f13b6c10028b29
chengzhinei <chengxue5yuan@163.com>
2025-08-27 16:23:27 +0800

    fix humantime config

--- app/src/app_config.rs
+++ app/src/app_config.rs
@@ -14,6 +14,7 @@ pub struct EngineConfig {
     /// path of jwtsecret
     pub wt_path: String,
     /// block interval time
+    #[serde(with = "humantime_serde")]
     pub block_interval: Duration,
 }
 
